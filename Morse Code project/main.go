5eee356d7289529546cfac54ab0719fc570ffa4e
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-05-10 11:39:26 +0200

    Morse code project

--- Morse Code project/main.go
+++ Morse Code project/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"morse/converter"
+	"os"
+	"strings"
+)
+
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Morse code converter, use characters that are found in the International Morse table.")
+	fmt.Print("Type in the message you want to see in Morse code: ")
+	message, _ := reader.ReadString('\n')
+	converter.Converter(strings.TrimSpace(strings.ToUpper(message)))
 
 }
